e1caf0736a7c2a4e99110a0d129020656a8e8654
amiryan23 <22mayk222@gmail.com>
2024-12-07 18:20:00 +0400

    Version 1.0.2 fix

--- app.js
+++ app.js
@@ -37,39 +37,40 @@ app.use('/api',apiRoutes)
 
 let bot = new Telegraf(process.env.BOT_TOKEN)
 
-bot.start((ctx) => {
-  ctx.replyWithPhoto({ url: 'https://i.ibb.co/SBgmpWB/2.png' }, {
-    caption: 'Welcome to our game! Here you can find exciting adventures and quests. Ready to play?',
-    reply_markup: {
-      inline_keyboard: [
-        [{ text: 'Start', url: 'https://t.me/santaquest_bot/santaquest' }],
-        [{ text: 'Join Community', url: 'https://t.me/santaquest' }]
-      ]
-    }
-  });
-});
+// bot.start((ctx) => {
+//   ctx.replyWithPhoto({ url: 'https://i.ibb.co/SBgmpWB/2.png' }, {
+//     caption: 'Welcome to our game! Here you can find exciting adventures and quests. Ready to play?',
+//     reply_markup: {
+//       inline_keyboard: [
+//         [{ text: 'Start', url: 'https://t.me/santaquest_bot/santaquest' }],
+//         [{ text: 'Join Community', url: 'https://t.me/santaquest' }]
+//       ]
+//     }
+//   });
+// });
+
+
 
 bot.on('pre_checkout_query', (ctx) => {
-  ctx.answerPreCheckoutQuery(true);
-});
+    ctx.answerPreCheckoutQuery(true)
+})
 
 bot.on('message', (ctx) => {
-  if (ctx.update.message.successful_payment !== undefined) {
-    ctx.reply('Thanks for the purchase!');
-  } else {
-    // Handle other message types, subtypes
-  }
-});
+    if (ctx.update.message.successful_payment != undefined) {
+        ctx.reply('Thanks for the purchase!')
+    } else {
+        // Handle other message types, subtypes
+    }
+})
+
+bot.launch()
+  .then(() => console.log('Бот запущен!'))
+  .catch((error) => console.error('Ошибка при запуске бота:', error));
 
 
-const PORT = process.env.PORT;
-const WEBHOOK_URL = process.env.WEBHOOK_URL ;
 
-bot.telegram.setWebhook(`${WEBHOOK_URL}/bot${process.env.BOT_TOKEN}`);
-app.use(bot.webhookCallback(`/bot${process.env.BOT_TOKEN}`));
-
-
-app.listen(PORT, '0.0.0.0', () => {
+const PORT = process.env.PORT ;
+app.listen(PORT,'0.0.0.0', () => {
   console.log(`Server is running on port ${PORT}`);
-  console.log(`Webhook set to: ${WEBHOOK_URL}/bot${process.env.BOT_TOKEN}`);
+
 });
